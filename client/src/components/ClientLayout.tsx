b5debd83dfa87ec5368247ec683ba50195e29e67
timalwin10-netizen <timalwin10@gmail.com>
2026-01-02 07:07:47 +0700

    Add Blackbird style welcome screen

--- client/src/components/ClientLayout.tsx
+++ client/src/components/ClientLayout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import WelcomeScreen from '@/components/WelcomeScreen';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
@@ -8,6 +9,7 @@ import { ReactNode } from 'react';
 export default function ClientLayout({ children }: { children: ReactNode }) {
     return (
         <ThemeProvider>
+            <WelcomeScreen />
             <Navbar />
             <main className="flex-grow relative z-10">
                 {children}
