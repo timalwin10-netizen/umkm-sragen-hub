3790879aaacf3abdf01b083c7cef2bd0ee48cd70
timalwin10-netizen <timalwin10@gmail.com>
2026-01-02 13:16:03 +0700

    Remove WelcomeScreen and simplify layout for SEO deployment

--- client/src/components/ClientLayout.tsx
+++ client/src/components/ClientLayout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import WelcomeScreen from '@/components/WelcomeScreen';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
@@ -9,7 +8,6 @@ import { ReactNode } from 'react';
 export default function ClientLayout({ children }: { children: ReactNode }) {
     return (
         <ThemeProvider>
-            <WelcomeScreen />
             <Navbar />
             <main className="flex-grow relative z-10">
                 {children}
